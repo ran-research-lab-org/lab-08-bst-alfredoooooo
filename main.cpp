ee2219d8cfea7cbe19c437d4776ea5d2f6ed3d5c
AlfredoFrontera <alfredo.frontera2@upr.edu>
2025-11-07 04:02:49 +0000

    Alfredo Frontera 801-21-1568 - Lab 8 completed

--- main.cpp
+++ main.cpp
@@ -1,30 +1,70 @@
+// Alfredo F. Frontera Del Valle
+// Lab 08
+// 801-21-1568
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "BinarySearchTree.hpp"
 #include "doctest.h"
-#include <sstream>
-#include <stdexcept>
+#include "BinarySearchTree.hpp"
 
-using namespace std;
+TEST_CASE("Testing insert and contains") {
+    BinarySearchTree<int> tree;
+    
+    CHECK(tree.toInorderStr() == "");
+    
+    tree.insert(8);
+    CHECK(tree.contains(8) == true);
+    CHECK(tree.toInorderStr() == "8");
+    
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(1);
+    tree.insert(6);
+    CHECK(tree.toInorderStr() == "1 3 6 8 10");
+    CHECK(tree.contains(6) == true);
+    CHECK(tree.contains(99) == false);
+}
 
-TEST_CASE("BinarySearchTree basic operations") {
-  BinarySearchTree<int> B;
-  B.insert(10);
+TEST_CASE("Testing remove") {
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(1);
+    
+    tree.remove(1);
+    CHECK(tree.contains(1) == false);
+    CHECK(tree.toInorderStr() == "3 8 10");
+}
 
-  B.insert(4);
-  B.insert(2);
-  B.insert(15);
-  B.insert(31);
-  B.insert(1);
-  B.insert(56);
-  CHECK(B.toInorderStr() == "1,2,4,10,15,31,56");
-  CHECK(B.BFT() == "[[10],[4,15],[2,31],[1,56]]");
-  BinarySearchTree<int> C;
-  for (auto e :
-       {16, 8, 24, 28, 20, 12, 4, 2, 6, 10, 14, 18, 22, 26, 30, 15, 7, 3}) {
-    C.insert(e);
-  }
+TEST_CASE("Testing BFT - Tree A") {
+    BinarySearchTree<int> tree;
+    
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(10);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(14);
+    tree.insert(4);
+    tree.insert(7);
+    tree.insert(13);
+    
+    CHECK(tree.BFT() == "[[8],[3,10],[1,6,14],[4,7,13]]");
+}
 
-  CHECK(C.toInorderStr() == "2,3,4,6,7,8,10,12,14,15,16,18,20,22,24,26,28,30");
-  CHECK(C.BFT() ==
-        "[[16],[8,24],[4,12,20,28],[2,6,10,14,18,22,26,30],[3,7,15]]");
+TEST_CASE("Testing BFT - Tree B") {
+    BinarySearchTree<int> tree;
+    
+    tree.insert(6);
+    tree.insert(2);
+    tree.insert(8);
+    tree.insert(7);
+    tree.insert(9);
+    
+    CHECK(tree.BFT() == "[[6],[2,8],[7,9]]");
+}
+
+TEST_CASE("Testing BFT - Empty tree") {
+    BinarySearchTree<int> tree;
+    CHECK(tree.BFT() == "[]");
 }
